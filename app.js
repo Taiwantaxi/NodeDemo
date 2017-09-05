2be135cf90e7dab7ada9a529f072e6f2449a3383
Bo-Rong <brown.huang@taiwantaxi.com.tw>
2017-09-05 14:16:42 +0800

    commit

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var app = require('express')();
 
+const PORT = process.env.PORT || 3000
+
 app.get('/', function(req, res){
   console.log(req.body);
   res.send('Hello World');
 });
 
-app.listen(3000, function(){
-  console.log('Listening on *:3000');
+app.listen(PORT, function(){
+  console.log(`App listening on port ${PORT}!`)
 });
 
 
